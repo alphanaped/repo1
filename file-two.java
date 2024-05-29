d836ea41037d4c60f4a647d4d2f3fb048b43a7c3
alphanaped <160116198+alphanaped@users.noreply.github.com>
2024-05-29 09:27:20 +0000

    user data

--- file-two.java
+++ file-two.java
@@ -1,5 +1,7 @@
 class Demo{
     public static void main(String[] arg){
         System.out.println("Well come to online codding experience");
+        byte age = 2;
+        String userName = "this is user name and its info";
     }
 }
