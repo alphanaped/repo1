3e0f2453763669f72219ca8de2dc3f0346ecf598
miatmi <alphanaped@gmail.com>
2024-06-04 17:54:46 +0500

    list identifier of data types on local env

--- Dtype.java
+++ Dtype.java
@@ -1,5 +1,10 @@
 class Dtype{
     public static void main(String[] arg){
         System.out.println("data type list in java");
+	byte flag = 1;
+	short little = 322;
+	int moderate = 3234;
+	long handsome = 3234234L;
+
     }
 }
