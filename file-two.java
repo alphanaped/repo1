dec91cdc5c80351ec997d390f5509481b2d9bc85
miatmi <alphanaped@gmail.com>
2024-06-04 17:34:11 +0500

    amount was added in file-one java

--- file-two.java
+++ file-two.java
@@ -3,5 +3,6 @@ class Demo{
         System.out.println("Well come to online codding experience");
         byte age = 2;
         String userName = "this is user name and its info";
+	short ammount = 3223;
     }
 }
